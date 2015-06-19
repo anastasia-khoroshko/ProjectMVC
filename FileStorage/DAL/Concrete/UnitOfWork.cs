cbd734cc5040ee66ab4e2f1edc1d63a6a44e74ba
anastasia-khoroshko <anastasia.khoroshko@gmail.com>
2015-06-19 09:03:39 +0300

    Refactoring
    Refactoring

--- FileStorage/DAL/Concrete/UnitOfWork.cs
+++ FileStorage/DAL/Concrete/UnitOfWork.cs
@@ -13,25 +13,25 @@ namespace DAL.Concrete
     public class UnitOfWork : IUnitOfWork,IDisposable
     {
         private readonly DbContext Context;
-        private IDictionary<Type, object> repositories;
+        private readonly IDictionary<Type, object> repositories;
         public UnitOfWork(DbContext context)
         {
             Context = context;
             repositories = new Dictionary<Type, object>();
-            RegisterRepository(new UserRepository(Context));
-            RegisterRepository(new RoleRepository(Context));
-            RegisterRepository(new PostRepository(Context));
-            RegisterRepository(new CategoryRepository(Context));
-            RegisterRepository(new CommentRepository(Context));
-            RegisterRepository(new ProfileRepository(Context));
-            RegisterRepository(new VoteRepository(Context));            
+            Register(new UserRepository(Context));
+            Register(new RoleRepository(Context));
+            Register(new PostRepository(Context));
+            Register(new CategoryRepository(Context));
+            Register(new CommentRepository(Context));
+            Register(new ProfileRepository(Context));
+            Register(new VoteRepository(Context));            
         }
 
         public IRepository<T> GetRepository<T>() where T : IEntity
         {
             return (IRepository<T>)repositories[typeof(T)];
         }
-        private void RegisterRepository<T>(IRepository<T> repository) where T : IEntity
+        private void Register<T>(IRepository<T> repository) where T : IEntity
         {
             repositories[typeof(T)] = repository;
         }
